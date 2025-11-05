fea3450555d0aa245d3fdaf18affc0b1f40cf84e
KostasEreksonas <k.ereksonas@gmail.com>
2025-11-05 11:48:18 +0200

    Updates to PoC interface

--- js/websocket.js
+++ js/websocket.js
@@ -1,32 +1,62 @@
-const flow_counts = {}; // Data to graph how many flows were instantiated from which IP address
+const src_flow_counts = {}; // Flow counts instantiated by source IP
+const dst_flow_counts = {}; // Flow counts instantiated by destination IP
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const config = {
+const source_flow_graph = document.getElementById('src_chart').getContext('2d');
+const destination_flow_graph = document.getElementById('dst_chart').getContext('2d');
+
+const source_config = {
 	type: 'bar',
 	data: {
 		labels: [],  // Source IP addresses
 		datasets: [{
 			label: 'Flow Count',
 			data: [],  // Counts per IP
-			backgroundColor: 'rgba(75, 192, 192, 0.2)',
-			borderColor: 'rgba(75, 192, 192, 1)'
+			backgroundColor: 'rgba(56, 48, 191, 0.8)',
+			borderColor: 'rgba(27, 6, 11, 0.8)'
 		}]
 	}
 }
 
-const myChart = new Chart(ctx, config);
+const destination_config = {
+	type: 'bar',
+	data: {
+		labels: [],  // Source IP addresses
+		datasets: [{
+			label: 'Flow Count',
+			data: [],  // Counts per IP
+			backgroundColor: 'rgba(56, 48, 191, 0.8)',
+			borderColor: 'rgba(27, 6, 11, 0.8)'
+		}]
+	}
+}
 
-function updateChart(src_ip, count){
-	const index = myChart.data.labels.indexOf(src_ip);
+const src_chart = new Chart(source_flow_graph, source_config);
+const dst_chart = new Chart(destination_flow_graph, destination_config);
+
+function update_src_chart(src_ip, count){
+	const index = src_chart.data.labels.indexOf(src_ip);
 
 	if (index === -1) {
-		myChart.data.labels.push(src_ip);
-    		myChart.data.datasets[0].data.push(count);
+		src_chart.data.labels.push(src_ip);
+    	src_chart.data.datasets[0].data.push(count);
 	} else {
-		myChart.data.datasets[0].data[index] = count;
+		src_chart.data.datasets[0].data[index] = count;
 	}
 
-	myChart.update();
+	src_chart.update();
+}
+
+function update_dst_chart(dst_ip, count){
+	const index = dst_chart.data.labels.indexOf(dst_ip);
+
+	if (index === -1) {
+		dst_chart.data.labels.push(dst_ip);
+    	dst_chart.data.datasets[0].data.push(count);
+	} else {
+		dst_chart.data.datasets[0].data[index] = count;
+	}
+
+	dst_chart.update();
 }
 
 var ws = new WebSocket("ws://localhost:8000/ws");
@@ -36,7 +66,7 @@ ws.onopen = function(event) {
 };
 
 ws.onmessage = function(event) {
-	data = JSON.parse(event.data);	
+	data = JSON.parse(event.data);
 
 	let date = new Date(data['termination'] * 1000);
 	let hours = date.getHours();
@@ -45,7 +75,7 @@ ws.onmessage = function(event) {
 	let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)
 
 	let table = document.getElementById("flow");
-	let row = table.insertRow();
+	let row = table.insertRow(0); // Latest flow records will be inserted at the top of the table
 	let flow_id = row.insertCell(0);
 	let timestamp = row.insertCell(1);
 	let source = row.insertCell(2);
@@ -80,13 +110,20 @@ ws.onmessage = function(event) {
 	extended_stats.appendChild(button);
 
 	src_ip = data['original_flow_key'][0];
-	if (src_ip in flow_counts) {
-		flow_counts[src_ip] += 1;
+	if (src_ip in src_flow_counts) {
+		src_flow_counts[src_ip] += 1;
 	} else {
-		flow_counts[src_ip] = 1;
+		src_flow_counts[src_ip] = 1;
 	}
+	update_src_chart(src_ip, src_flow_counts[src_ip]);
 
-	updateChart(src_ip, flow_counts[src_ip]);
+	dst_ip = data['original_flow_key'][1];
+	if (dst_ip in dst_flow_counts) {
+		dst_flow_counts[dst_ip] += 1;
+	} else {
+		dst_flow_counts[dst_ip] = 1;
+	}
+	update_dst_chart(dst_ip, dst_flow_counts[dst_ip]);
 };
 
 ws.onclose = function(event) {
@@ -104,11 +141,13 @@ function openModal(flowData) {
 
 	let content = `<h3>Flow ID: ${flowData['flowId']}</h3>`;
 
-	content += "<h1>Attack Probabilites</h1>";
-	content += "<table class=table>";
+    content += "<div class=\"row\">";
+
+    content += "<div class=\"col-md-6\">";
+	content += "<h1 class=\"text-center\">Attack Probabilites</h1>";
+	content += "<table class=\"table table-bordered table-striped table-hover\">";
 	content += "<thead>";
-	content += "<tr><th>Attack</th>";
-	content += "<th>Probability</th></th>";
+	content += "<tr><th scope=\"col\">Attack</th><th scope=\"col\">Probability</th></th>";
 	content += "</thead>";
 	content += "<tbody>";
 	for (const key in results){
@@ -116,12 +155,13 @@ function openModal(flowData) {
 	}
 	content += "</tbody>";
 	content += "</table>";
+    content += "</div>";
 
-	content += "<h1>Statistical Features</h1>";
-	content += "<table class=table>";
+    content += "<div class=\"col-md-6\">";
+	content += "<h1 class=\"text-center\">Statistical Features</h1>";
+	content += "<table class=\"table table-bordered table-striped table-hover\">";
 	content += "<thead>";
-	content += "<tr><th>Feature Name</th>";
-	content += "<th>Value</th></th>";
+	content += "<tr><th scope=\"col\">Feature Name</th><th scope=\"col\">Value</th></th>";
 	content += "</thead>";
 	content += "<tbody>";
 	for (const key in stats){
@@ -129,6 +169,9 @@ function openModal(flowData) {
 	}
 	content += "</tbody>";
 	content += "</table>";
+	content += "</div>";
+
+	content += "</div>";
 
 	modalContent.innerHTML = content;
 	modal.style.display = 'block';
